501ed35e7eeef314e40bad801259efefe53a811c
Yogendra Rampuria (Yogi) <yrampuria@pivotal.io>
2019-12-24 21:09:23 +0800

    add download-images.sh

---
+++ bin/download-images.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+FILE=${1:-}
+[[ -z $FILE ]] && echo "File name not given" && exit 1
+
+FILE_NAME=$(basename $FILE)
+FILE_NAME_NO_EXT=${FILE_NAME%%.*}
+FILE_DIR=$(dirname $FILE)
+ASSET_DIR=$FILE_DIR/$FILE_NAME_NO_EXT
+
+echo mkdir -p $ASSET_DIR
+
+grep -Eo https\?://goo.gl/.\\S+  $FILE | sort | uniq | while read url
+do
+echo Download $url
+done
