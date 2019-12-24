4b4e9d44e1ffc4d2109dc09b5fdf18272fea64f4
Yogendra Rampuria (Yogi) <yrampuria@pivotal.io>
2019-12-24 23:03:00 +0800

    Fixed location calculation for easier migration

--- bin/download-images.sh
+++ bin/download-images.sh
@@ -2,15 +2,20 @@
 
 FILE=${1:-}
 [[ -z $FILE ]] && echo "File name not given" && exit 1
+[[ ! -f $FILE ]] && echo "$FILE: Not a reguler file" && exit 1
 
 FILE_NAME=$(basename $FILE)
 FILE_NAME_NO_EXT=${FILE_NAME%%.*}
-FILE_DIR=$(dirname $FILE)
+FILE_DIR=$(cd `dirname $FILE`|pwd)
 ASSET_DIR=$FILE_DIR/$FILE_NAME_NO_EXT
-
+echo "$FILE_NAME: Creating Asset Dir ${ASSET_DIR}"
 mkdir -p $ASSET_DIR
+
+echo $FILE_NAME: Copy asset dir location
+echo $ASSET_DIR | pbcopy
 
 grep -Eo https\?://goo.gl/.\\S+  $FILE | sort | uniq | while read url
 do
+  echo "$FILE_NAME :: $url :: Launched browser"
   (cd $ASSET_DIR && open $url)
 done
